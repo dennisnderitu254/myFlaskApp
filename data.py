8f5503b0f7c041180c1210a402ced0b5bada2ad2
Dennis Nderitu <dknderitu@gmail.com>
2017-08-05 15:55:51 +0300

    Completed phase one of the myFlaskApp

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+	articles = [
+		{
+		   'id':1,
+		   'title':'Article One',
+		   'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+		   'author':'Dennis Nderitu',
+		   'create_date':'5-8-2017'
+
+		},
+
+		{
+			'id':2,
+		   'title':'Article two',
+		   'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+		   'author':'Lonzo Ball',
+		   'create_date':'6-8-2017'
+
+
+		},
+
+		{
+			'id':3,
+		   'title':'Article Three',
+		   'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+		   'author':'King Bask',
+		   'create_date':'7-8-2017'
+		}
+
+	]
+
+	return articles
